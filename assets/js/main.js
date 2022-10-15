f4678d210ffda3a18e117cd23052cb87e5183fcb
Jhonatan Atuesta <jdatuesta7@gmail.com>
2022-10-14 22:41:27 -0500

    Updates

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+import Swiper from '../../../node_modules/swiper/swiper-bundle.esm.browser.min.js';
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('navbar')
+    if (this.scrollY >= 1) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SWIPER ===============*/
+
+const mySwiper = new Swiper(".my-swiper", {
+    spaceBetween: 0,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 5,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 7,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 10,
+        },
+      },
+});
+
+const tourSwiper = new Swiper(".tour-swiper", {
+  spaceBetween: 0,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 5,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 7,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+});
+
+const testimonialsSwiper = new Swiper(".swiper-testimonials", {
+  effect: "cards",
+  grabCursor: true,
+});
